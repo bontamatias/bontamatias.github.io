aa587d1ecc2efed8822aa8f26d2d2ee2b9321e5b
Matias Bonta <bontamatias@gmail.com>
2018-11-21 17:56:51 -0600

    console stuff

--- navigate.js
+++ navigate.js
@@ -1,15 +1,21 @@
 $(function(e) {
 
-  //initial setup of 
-  var tabs = $(".tabs");
+  var tabs = $(".tabs").find(".tab");
+  var pages = $(".scroller").find(".page");
+  console.log("loaded tabs: 0-" + (tabs.length-1));
+  console.log("loaded pages: 0-" + (pages.length-1));
+
+  //initial setup of menu bar selector position
   var selector = $(".tabs").find(".selector");
-  var activeTab = tabs.find(".active");
+  var activeTab = $(".tabs").find(".active");
   $(".selector").css({
     left: activeTab.position.left + "px",
     width: activeTab.innerWidth() + "px"
   });
 
   $(".tabs").on("click", "a", function() {
+    //change selector position on click
+    console.log("tab: " + tabs.index(this));
     $(".tabs a").removeClass("active");
     $(this).addClass("active");
     $(".selector").css({
@@ -17,11 +23,15 @@ $(function(e) {
       width: $(this).innerWidth() + "px"
     });
 
+    //scroll page on click
+
     $(".scroller page").removeClass("active");
-    $(this).addClass("active");
+    $(pages[tabs.index(this)]).addClass("active");
     $(".scroller").css({
         top: $(".products").innerHeight + "vh",
     });
+    console.log(tabs);
+    console.log(pages);
 
   });
 
@@ -29,5 +39,6 @@ $(function(e) {
   /*$(".scroller").css({
     top: -100 + "vh",
   });*/
+
 
 });
