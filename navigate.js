c0a956b83f20b60f749917f0119f8fd31e3304cb
Matias Bonta <bontamatias@gmail.com>
2019-03-09 12:02:05 -0600

    scrollwheel test 1.1

--- navigate.js
+++ navigate.js
@@ -154,7 +154,7 @@
   });
 
   //scrollwheel test
-  $('#foo').bind('mousewheel', function(e){
+  $('.scroller').bind('mousewheel', function(e){
     if(e.originalEvent.wheelDelta /120 > 0) {
       alert('up');
     }
